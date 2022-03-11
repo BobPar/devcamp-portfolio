792b71800f3c926b17a509c96dc6672dec86a879
BobParr <sugada.katsuhiro@gmail.com>
2022-03-11 16:07:48 +0900

    chap12

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'devise', '~> 4.8', '>= 4.8.1'
 gem 'bootstrap', '~> 5.1.3'
 gem 'jquery-rails'
 
+gem 'petergate', '~> 3.0'
+
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = KatsuDevcampViewTool::Renderer.copyright("Katsuhiro", "All rihgts reserved")
+  end
+end
+
+module KatsuDevcampViewTool
+  class Renderer
+    def self.copyright(name, msg)
+      "&copy; #{Time.current.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
